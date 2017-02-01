f079944c902e3603a281fd945946a3103ec3fa68
JeanLLopes <jean.llopes@outlook.com>
2017-01-31 22:56:03 -0200

    Add DbSet

---
+++ Code-First-com-ASP.NET-MVC/Context/EFContext.cs
@@ -0,0 +1,31 @@
+﻿using Code_First_com_ASP.NET_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Code_First_com_ASP.NET_MVC.Context
+{
+    public class EFContext : DbContext
+    {
+        //ctor
+        public EFContext() : base("EntitiFramewordConnection")
+        {
+            
+            
+        }
+
+        public DbSet<UniversidadeModel> Universidades { get; set; }
+        public DbSet<CursoModel> Cursos { get; set; }
+        public DbSet<AlunoModel> Alunos { get; set; }
+
+        //DESABILITAR A PLURALIZAÇAÕ DOS TABELAS QUE SERÃO CRIADAS
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
